9d40b41fb52891c2f8b335b3398e064e718e7697
Himadri Talukder <himadri@digitalmenubox.com>
2013-03-14 16:32:55 -0400

    - inital commit from office

---
+++ src/tek/chainofresponsibility/Chain.java
@@ -0,0 +1,16 @@
+package tek.chainofresponsibility;
+
+/* Chain of responsibility is a design pattern where a sender sends a request to a chain of objects, where the objects in 
+ * the chain decide themselves who to honor the request. If an object in the chain decides not to serve the request, it 
+ * forwards the request to the next object in the chain.*/
+
+public interface Chain {
+
+	public abstract void setNext(Chain nextInChain);
+
+	public abstract void process(Number request);
+}
+
+/* There is a possibility where none of the node may serve the request
+ * If the chain becomes too long there is a possibility of messing up the code*/
+ 
